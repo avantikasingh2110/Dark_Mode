085c094a905509d4c4b7d51db2b5e3f066aa5bc7
avantikasingh2110 <avantikasingh.2110@gmail.com>
2024-10-28 22:53:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let body = document.querySelector("body");
+let toggle_btn_container = document.getElementById("toggle_btn_container");
+let circle_btn = document.getElementById("circle_btn");
+let h1 = document.querySelector("h1");
+
+let count = 0;
+
+toggle_btn_container.addEventListener("click", function(){
+    if(count % 2 == 0){
+        circle_btn.style.marginLeft = "0";
+        count++;
+        body.style.backgroundColor = "white";
+        h1.style.color = "black";
+    } else {
+        circle_btn.style.marginRight = "0";
+        count--;
+        body.style.backgroundColor = "black";
+        h1.style.color = "white";
+    }
+})
+
+
+
+
+
+// toggle_btn_container.addEventListener("click", function(){
+//         if(count%2==0){
+//             circle_btn.style.marginLeft="0";
+//              count++;
+//              body.style.backgroundColor = "white";
+//              h1.style.filter="invert(0)";
+//         } else {
+//             circle_btn.style.marginLeft="70px";
+//              count++;
+//              body.style.backgroundColor = "black";
+//              h1.style.filter="invert(1)";
+//         }
+// })
